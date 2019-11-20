a14d9ec9e4601ca458aca2a33f94b18a1cfb8ce4
authentical <cvspencer@gmail.com>
2019-11-20 19:50:46 -0400

    Fixed window appearing in lower-right in >1080p displays

--- desktop/src/com/potatospy/desktop/DesktopLauncher.java
+++ desktop/src/com/potatospy/desktop/DesktopLauncher.java
@@ -4,11 +4,21 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.potatospy.KanaBucket;
 
+
+// DesktopLauncher binds application Height Width and x y position
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// Application Height Width and x y position
 		config.width = KanaBucket.APP_WIDTH;
 		config.height = KanaBucket.APP_HEIGHT;
+
+		config.x=-1;config.y=-1; // Centre window on screen
+
+
 		new LwjglApplication(new KanaBucket(), config);
 	}
 }
